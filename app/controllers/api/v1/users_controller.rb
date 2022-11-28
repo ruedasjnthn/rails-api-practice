475ef67f8be2e2e97b7dff0be8fa2d7735727e89
jaydi <ruedasjnthn@gmail.com>
2022-11-28 20:20:51 +0800

    Adds update action the users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::UsersController < ApplicationController
-  before_action :set_user, only: %i[show update]
+  before_action :set_user, only: %i[show update destroy]
+
   # GET /users/1
   def show
     render json: @user
@@ -21,6 +22,12 @@ class Api::V1::UsersController < ApplicationController
     else
       render json: @user.errors, status: :unprocessable_entity
     end
+  end
+
+  # DELETE /users/1
+  def destroy
+    @user.destroy
+    head 204
   end
 
   private
